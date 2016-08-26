7f0f65df47cb4c616a473805283098bd10a7d980
Louis.L <qingyanjiu@126.com>
2016-08-26 15:03:36 +0800

    修改compose配置

--- conf/db.js
+++ conf/db.js
@@ -1,22 +1,32 @@
 // MySQL数据库联接配置
 module.exports = {
-//   mysql: {
-//     host: '192.168.99.100',
-//     user: 'root',
-//     password: '123',
-//     database:'livestream', 
-//     port: 3306,
-//     connectionLimit: 10,
-//     supportBigNumbers: true
-//   }
+  // mysql: {
+  //   host: '192.168.99.100',
+  //   user: 'root',
+  //   password: '123',
+  //   database:'livestream',
+  //   port: 3306,
+  //   connectionLimit: 10,
+  //   supportBigNumbers: true
+  // }
+
+    mysql: {
+        host: 'srs-db',
+        user: 'root',
+        password: '123',
+        database:'livestream',
+        port: 3306,
+        connectionLimit: 10,
+        supportBigNumbers: true
+    }
   
-   mysql: {
-    host: '10.10.11.174',
-    user: 'u6RGBpsT3ZjkqHrY',
-    password: 'pNenQbWCifaOspqVj',
-    database:'kCr8lUzcWhJoSV3Z', 
-    port: 3306,
-    connectionLimit: 10,
-    supportBigNumbers: true
-  }
+  //  mysql: {
+  //   host: '10.10.11.174',
+  //   user: 'u6RGBpsT3ZjkqHrY',
+  //   password: 'pNenQbWCifaOspqVj',
+  //   database:'kCr8lUzcWhJoSV3Z',
+  //   port: 3306,
+  //   connectionLimit: 10,
+  //   supportBigNumbers: true
+  // }
 };
