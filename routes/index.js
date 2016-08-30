a40d2bae92eaa7257926a09475f06e7e60a782fa
Louis.L <qingyanjiu@126.com>
2016-08-30 11:46:43 +0800

    添加了初始化数据库功能

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,116 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var InitDatabase = require('../services/InitDatabase');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    if (req.session.userid) {
-        res.redirect("/live");
-    }
-    else {
-        var json = {"title": 'PRIVATE直播', "streamUrl": "10.204.21.34", "streamName": "live", "streamCode": "livestream"}
-        res.render('index', json);
-    }
+    //先看看有没有初始化过数据库
+    var connection = mysql.createConnection({
+        host: '192.168.99.100',
+        user: 'root',
+        password: '123'
+    });
+    connection.connect();
+    connection.query('show databases', function (err, data) {
+        if (err)
+            throw err;
+        //如果没有livestream这个数据库，说明没有初始化
+        if (JSON.stringify(data).indexOf("livestream") == -1) {
+            res.redirect("/init");
+        }
+        else {
+            //如果已经有数据库，正常进入系统
+            if (req.session.userid) {
+                res.redirect("/live");
+            }
+            else {
+                var json = {
+                    "title": 'PRIVATE直播',
+                    "streamUrl": "10.204.21.34",
+                    "streamName": "live",
+                    "streamCode": "livestream"
+                }
+                res.render('index', json);
+            }
+        }
+    });
+    connection.end();
+
+});
+
+
+router.get('/init', function (req, res, next) {
+    //先看看有没有初始化过数据库
+    var connection = mysql.createConnection({
+        host: '192.168.99.100',
+        user: 'root',
+        password: '123'
+    });
+    connection.connect();
+    connection.query('show databases', function (err, data) {
+        if (err)
+            throw err;
+        //如果没有livestream这个数据库，说明没有初始化
+        if (JSON.stringify(data).indexOf("livestream") == -1) {
+            res.render("init");
+        }
+        else {
+            //如果已经有数据库， 就直接进入系统，不能随便初始化
+            if (req.session.userid) {
+                res.redirect("/live");
+            }
+            else {
+                var json = {
+                    "title": 'PRIVATE直播',
+                    "streamUrl": "10.204.21.34",
+                    "streamName": "live",
+                    "streamCode": "livestream"
+                }
+                res.render('index', json);
+            }
+        }
+    });
+    connection.end();
+});
+
+router.post('/initDatabase', function (req, res, next) {
+    //先看看有没有初始化过数据库
+    var connection = mysql.createConnection({
+        host: '192.168.99.100',
+        user: 'root',
+        password: '123'
+    });
+    connection.connect();
+    connection.query('show databases', function (err, data) {
+        if (err)
+            throw err;
+        //如果没有livestream这个数据库，说明没有初始化
+        if (JSON.stringify(data).indexOf("livestream") == -1) {
+            InitDatabase.initDatabase({}, function (err, data) {
+                if (err)
+                    res.json({"result": "error"});
+                if (data)
+                    res.json(data);
+            });
+        }
+        else {
+            //如果已经有数据库， 就直接进入系统，不能随便初始化
+            if (req.session.userid) {
+                res.redirect("/live");
+            }
+            else {
+                var json = {
+                    "title": 'PRIVATE直播',
+                    "streamUrl": "10.204.21.34",
+                    "streamName": "live",
+                    "streamCode": "livestream"
+                }
+                res.render('index', json);
+            }
+        }
+    });
+    connection.end();
 });
 
 module.exports = router;
